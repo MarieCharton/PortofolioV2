bd35be76ada83bfbc358b8cf8d705d1b2f59ba61
marie <mariecharton21@gmail.com>
2021-03-29 15:56:30 +0200

    capctha2

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -120,7 +120,7 @@ class MainController extends AbstractController
         $response=json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LekypMaAAAAAHwl6yyrvx43TkjJLcPFnQw2EWNv&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']), true);
         if($response['success'] == false)
         {
-          echo '<h2>You are spammer ! Get the @$%K out</h2>';
+            
         }
         else
         {
