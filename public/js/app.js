262a43bdd835206a0880c5ea959ecd56c4c2a7b9
marie <mariecharton21@gmail.com>
2021-04-09 16:30:11 +0200

    correction backtotop button et add technologies

--- public/js/app.js
+++ public/js/app.js
@@ -1,42 +1,24 @@
-//Viser le bouton
-var mybutton = document.getElementById("scroll-to-top-button");
+//Gestion du Carrousel
+jQuery(document).ready(function($) {
 
-// Si on scroll a 20px du haut le bouton apparait
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// Au click on remonte
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-
-// Menu fixe sur la page Blog 
-
-// $(function(){
-//   // On recupere la position du bloc par rapport au haut du site
-//   var position_top_raccourci = $(".category-menu").offset().top;
+  //Scroll to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
   
-//   //Au scroll dans la fenetre on déclenche la fonction
-//   $(window).scroll(function () {
-  
-//   //si on a defile de plus de 150px du haut vers le bas
-//   if ($(this).scrollTop() > position_top_raccourci) {
-  
-//   //on ajoute la classe "fixNavigation" a <div id="navigation">
-//   $('.category-menu').addClass("fixNavigation"); 
-//   } else {
-  
-//   //sinon on retire la classe "fixNavigation" a <div id="navigation">
-//   $('.category-menu').removeClass("fixNavigation");
-//   }
-//   });
-//   });
+  // scroll body to 0px on click
+  $('#back-to-top').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 400);
+    return false;
+  });
+
+});
+
+
+
